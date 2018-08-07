afcfb1aac01060fbce2cc5952a544acdea0d8238
menomanabdulla <noman.fcibappy@gmail.com>
2018-08-07 14:03:21 +0600

    setup project

---
+++ src/component/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
